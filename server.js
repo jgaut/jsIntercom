9bd0b0e98bc47225c8dbf417dda01d584b66e858
Jérémy GAUTIER <gautierjeremy@gmail.com>
2016-02-13 09:38:11 +0100

    clean

--- server.js
+++ server.js
@@ -8,11 +8,8 @@ var map = new HashMap();
 
 var request = require('request');
 var gpio = require('rpi-gpio');
-//var gpio = require("gpio");
 var async = require('async'); 
 var log4js = require('log4js');
-
-var log4js = require('log4js'); 
 //var logger = log4js.getLogger();
 
 log4js.loadAppender('file');
@@ -78,7 +75,7 @@ gpio.on('change', function(channel, value) {
 
             
             logger.debug(dateRef+" < "+dateNow+" ? true");
-    	   request('https://maker.ifttt.com/trigger/ringIntercomNotif/with/key/'+properties.get('main.ifttt.key'), function (error, response, body) {
+    	   request(properties.get('main.ifttt.url')+properties.get('main.ifttt.key'), function (error, response, body) {
   		    if (!error && response.statusCode == 200) {
     			logger.debug(body) // Show the HTML for the Google homepage. 
   			}else{
@@ -95,9 +92,39 @@ gpio.on('change', function(channel, value) {
 	} 
 });
 
-var pin7 = 7;
-var pin16 = 16;
-var pin12 = 12;
+//+-----+-----+---------+------+---+--B Plus--+---+------+---------+-----+-----+
+//| BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
+//+-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
+//|     |     |    3.3v |      |   |  1 || 2  |   |      | 5v      |     |     |
+//|   2 |   8 |   SDA.1 |   IN | 1 |  3 || 4  |   |      | 5V      |     |     |
+//|   3 |   9 |   SCL.1 |   IN | 1 |  5 || 6  |   |      | 0v      |     |     |
+//|   4 |   7 | GPIO. 7 |  OUT | 0 |  7 || 8  | 1 | ALT0 | TxD     | 15  | 14  |
+//|     |     |      0v |      |   |  9 || 10 | 1 | ALT0 | RxD     | 16  | 15  |
+//|  17 |   0 | GPIO. 0 |   IN | 0 | 11 || 12 | 0 | IN   | GPIO. 1 | 1     | 18  |
+//|  27 |   2 | GPIO. 2 |   IN | 0 | 13 || 14 |   |      | 0v      |     |     |
+//|  22 |   3 | GPIO. 3 |   IN | 0 | 15 || 16 | 1 | OUT  | GPIO. 4 | 4   | 23  |
+//|     |     |    3.3v |      |   | 17 || 18 | 0 | IN   | GPIO. 5 | 5   | 24  |
+//|  10 |  12 |    MOSI |   IN | 0 | 19 || 20 |   |      | 0v      |     |     |
+//|   9 |  13 |    MISO |   IN | 0 | 21 || 22 | 0 | IN   | GPIO. 6 | 6   | 25  |
+//|  11 |  14 |    SCLK |   IN | 0 | 23 || 24 | 1 | IN   | CE0     | 10  | 8   |
+//|     |     |      0v |      |   | 25 || 26 | 1 | IN   | CE1     | 11  | 7   |
+//|   0 |  30 |   SDA.0 |   IN | 1 | 27 || 28 | 1 | IN   | SCL.0   | 31  | 1   |
+//|   5 |  21 | GPIO.21 |   IN | 1 | 29 || 30 |   |      | 0v      |     |     |
+//|   6 |  22 | GPIO.22 |   IN | 1 | 31 || 32 | 0 | IN   | GPIO.26 | 26  | 12  |
+//|  13 |  23 | GPIO.23 |   IN | 0 | 33 || 34 |   |      | 0v      |     |     |
+//|  19 |  24 | GPIO.24 |   IN | 0 | 35 || 36 | 0 | IN   | GPIO.27 | 27  | 16  |
+//|  26 |  25 | GPIO.25 |   IN | 0 | 37 || 38 | 0 | IN   | GPIO.28 | 28  | 20  |
+//|     |     |      0v |      |   | 39 || 40 | 0 | IN   | GPIO.29 | 29  | 21  |
+//+-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
+//| BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
+//+-----+-----+---------+------+---+--B Plus--+---+------+---------+-----+-----+
+
+//Colonne wPi
+var pin7 = 7; //Detection de sonnerie
+var pin16 = 16; //Sortie ouverture porte
+var pin12 = 12; //Entree ouverture porte
+
+//Init des dates
 var dateRef = new Date();
 var dateAuto = new Date();
 var dateNow;

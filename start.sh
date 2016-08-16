d5e526d3cd6f90aee8322292bbc978e7341356a6
Jérémy GAUTIER <gautierjeremy@gmail.com>
2016-08-16 16:18:02 +0200

    passage avec nodejs en global

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ps aux |grep -v grep|grep node
 sudo kill $(ps aux |grep -v grep|grep node|awk -F' ' '{print $2}')
-nohup sudo /home/pi/btsync/nodejs/node-v5.0.0-linux-armv6l/bin/node server.js &
+nohup sudo node server.js &
 ps aux |grep -v grep|grep node
